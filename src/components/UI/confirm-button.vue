4b11943c91c8160bb1dfa6d09a5a772a56c60d20
GambrinusUp <kotanffas@gmail.com>
2023-02-25 17:17:11 +0700

    Созданы два UI компонента Delete-button и Confirm-button, и глобально зарегистрированы

---
+++ src/components/UI/confirm-button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+      @click="confirmData()"
+      class="confirm-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "confirm-button",
+  methods: {
+    confirmData() {
+      this.$emit('confirm', "confirm some data");
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+.confirm-button{
+  width: 178px;
+  height: 50px;
+  background-color: #95EAB2;
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 25px;
+  line-height: 30px;
+  color: #407D9F;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import UIcomponents from '@/components/UI'
 
-createApp(App).use(store).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+const app = createApp(App)
+
+
+UIcomponents.forEach(component => {
+    app.component(component.name, component)
+})
+
+
+app.use(store).use(router).use(vuetify).mount('#app')
