ac82d2f2bfe89fa310b61e4dfccb30bca0efdc82
Bondarew Alexey <alexbondarew321@mail.ru>
2023-03-13 10:59:05 +0700

    Finaly

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,79 @@
 <template>
     <div class="app">
-        <head-menu />
-        <router-view />
+        <head-menu :labeltext = "email"/>
+        <router-view></router-view>
     </div>
 
 </template>
 
-<script setup>
-import HeadMenu from "@/components/head-menu.vue";
+
+<script>
+import PostFormVue from './components/PostForm.vue'
+import PostListVue from '@/components/PostList.vue'
+import HeadMenu from './components/Head-menu.vue'
+import ChangeSchedule from './components/Change-Schedule.vue'
+import axios from 'axios'
+import AuthorizationPage from '@/pages/authorization-page.vue'
+
+
+
+export default {
+    components: {
+        PostFormVue, PostListVue,
+        HeadMenu,
+        ChangeSchedule,
+        AuthorizationPage,
+     
+
+    },
+    computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+    data() {
+        return {
+
+            rank: 'Homeless',
+            email: 'alexbond@mail.ru',
+            authformview: false,
+    
+        }
+    },
+    methods: {
+        changeemail(newmail){
+            this.email = newmail;
+            this.rank = 'Student';
+            console.log(newmail);
+            console.log(555);
+        },
+
+       /*  logout: function() {
+            this.$store.dispatch("account/logout").then(() => {
+            this.$router.push("/authorization");
+            });
+        } */
+   
+
+    }
+}
+
 </script>
 
 <style>
-.app{
-    background: #d5fcff;
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
+
+* {
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+
 }
+
+
+body {
+    /*padding: 20px;*/
+    background-color: #f0fafd;
+}
+
+
 </style>
